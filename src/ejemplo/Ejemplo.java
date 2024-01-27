4d405c20fddcf92d09a30b12ed4e36344c923501
Abdel Gadiel Martinez Lassonde <abdel.g.martinez.l@gmail.com>
2024-01-27 15:08:09 -0500

    Inclusión de tipos de datos primitivos y las cadenas

--- src/ejemplo/Ejemplo.java
+++ src/ejemplo/Ejemplo.java
@@ -2,7 +2,41 @@ package ejemplo;
 
 public class Ejemplo {
     public static void main (String[] args) {
+        /*
+        Impresion de un saludo
+        en pantalla, nada complejo
+         */
         System.out.println("Que Xopa!");
+
+        // Tipos de datos enteros
+        byte byteVar = 127;
+        short shortVar = 32767;
+        int intVar = 2147483647;
+        long longVar = 9223372036854775807L;
+
+        // Tipos de datos decimales
+        float floatVar = 3.14f;
+        double doubleVar = 3.1416;
+
+        // Tipos de datos caracteres
+        char charVar = 'A';
+
+        // Clase para las cadenas
+        String stringVar = "Chacalaca";
+
+        // Tipos de datos booleanos
+        boolean booleanVar = true;
+
+        System.out.println("byte: " + byteVar);
+        System.out.println("short: " + shortVar);
+        System.out.println("int: " + intVar);
+        System.out.println("long: " + longVar);
+        System.out.println("float: " + floatVar);
+        System.out.println("double: " + doubleVar);
+        System.out.println("char: " + charVar);
+        System.out.println("boolean: " + booleanVar);
+        System.out.println("String: " + stringVar);
+
     }
 
 }
